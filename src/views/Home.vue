c8fac095af818ad35c3b359691723769e26ace59
Dawid Napora <dawnap24@gmail.com>
2020-08-30 11:22:02 +0200

    Change default components and routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+  .page.page--home
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from '@vue/composition-api'
 
-@Component({
-  components: {
-    HelloWorld,
-  },
+export default defineComponent({
+  setup() {
+    return {}
+  }
 })
-export default class Home extends Vue {}
 </script>
